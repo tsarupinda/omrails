6a56e8c6a994b6ecb0807255b962570f5480d770
Tendai Sarupinda <tisarupinda@gmail.com>
2023-07-13 11:29:35 +0100

    Added some pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  root 'pages#home'
+  get 'about' => 'pages#about'
+  get 'contactus' => 'pages#contactus'
 end
